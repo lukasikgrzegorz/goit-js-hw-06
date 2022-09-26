76e4ff3495a98867df5c3bb4c31dcbd3b21ab401
lukasikgrzegorz <71289836+lukasikgrzegorz@users.noreply.github.com>
2022-09-26 20:48:55 +0200

    finish 10 task

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,32 @@
+const inputNumber = document.querySelector('input');
+let amount = 0;
+const createBtn = document.querySelector('[data-create]');
+const destroyBtn = document.querySelector('[data-destroy]');
+const boxes = document.querySelector('#boxes');
+
+inputNumber.addEventListener('change', (e) => {
+  amount = e.currentTarget.value;
+})
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+};
+
+createBtn.addEventListener('click', () => {
+  for (let i = 0; i < amount; i++){
+    const box = document.createElement("div");
+    box.style.width = 30 + 10 * i + "px";
+    box.style.height = 30 + 10 * i + "px";
+    const color = getRandomHexColor();
+    box.style.backgroundColor = color;
+    boxes.append(box);
+  }
+})
+
+destroyBtn.addEventListener('click', () => {
+  boxes.innerHTML = "";
+})
+
+
+
+
