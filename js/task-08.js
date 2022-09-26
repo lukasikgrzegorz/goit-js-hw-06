9bebcc7f725fab749089ad1b18caf428e408d58a
lukasikgrzegorz <71289836+lukasikgrzegorz@users.noreply.github.com>
2022-09-26 08:35:48 +0200

    task 8 finish

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,27 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    
+    const {
+        elements: { email, password }
+    } = e.currentTarget;
+
+    if (email.value === "" || password.value === "") {
+        return alert("Uzupełnij wszystkie pola!");
+    }
+
+    const outputArray = new Map([
+        [email.name, email.value],
+        [password.name, password.value]
+    ]);
+
+    console.log(outputArray);
+
+    const outputObject = Object.fromEntries(outputArray);
+
+    e.currentTarget.reset();
+
+    return console.log(outputObject);
+
+})
